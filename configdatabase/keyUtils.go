0dfdd733a78dd0b744187a7aa15c01827bdd94a3
PokornicSR01 <nemanja.pokornic02@gmail.com>
2023-05-23 21:53:40 +0200

    feat: change keyUtils

--- configdatabase/keyUtils.go
+++ configdatabase/keyUtils.go
@@ -6,51 +6,25 @@ import (
 )
 
 const (
-	config       = "config/%s/%s"
-	configLabels = "config/%s/%s/%s"
-	allConfigs   = "config/all"
-	group        = "group/%s/%s/config/%s"
-	groupLabels  = "group/%s/%s/%s/config/%s"
-	allGroups    = "group/all"
+	config = "config/%s/%s"
+	group  = "group/%s/%s/%s/config/%s"
 )
 
-func generateConfigKey(version string, labels string) (string, string) {
+func generateConfigKey(version string) (string, string) {
 	id := uuid.New().String()
-	if labels != "" {
-		return fmt.Sprintf(configLabels, id, version, labels), id
-
-	} else {
-		return fmt.Sprintf(config, id, version), id
-	}
+	return fmt.Sprintf(config, id, version), id
 }
 
-func constructConfigKey(id string, version string, labels string) string {
-
-	if labels != "" {
-		return fmt.Sprintf(configLabels, id, version, labels)
-
-	} else {
-		return fmt.Sprintf(config, id, version)
-	}
+func constructConfigKey(id string, version string) string {
+	return fmt.Sprintf(config, id, version)
 }
 
-func generateGroupKey(version string, labels string) (string, string) {
+func generateGroupKey(version string, labels string) (string, string, string) {
 	idGroup := uuid.New().String()
 	idConfig := uuid.New().String()
-	if labels != "" {
-		return fmt.Sprintf(groupLabels, idGroup, version, labels, idConfig), idGroup
-
-	} else {
-		return fmt.Sprintf(group, idGroup, version, idConfig), idGroup
-	}
+	return fmt.Sprintf(group, idGroup, version, labels, idConfig), idGroup, idConfig
 }
 
 func constructGroupKey(idGroup string, version string, labels string, idConfig string) string {
-
-	if labels != "" {
-		return fmt.Sprintf(groupLabels, idGroup, version, labels, idConfig)
-
-	} else {
-		return fmt.Sprintf(group, idGroup, version, idConfig)
-	}
+	return fmt.Sprintf(group, idGroup, version, labels, idConfig)
 }
